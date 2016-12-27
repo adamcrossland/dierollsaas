0428df9a8980d002a85fd355d0b2bb684c699492
ADAM-LAPTOP\adamc <adam.crossland@gmail.com>
2016-12-26 19:51:30 -0500

    A bunch of cosmetic changes to get golint off my ass

--- dierolsaas.go
+++ dierolsaas.go
@@ -1,4 +1,4 @@
-// derolsaas.go
+// dierolsaas.go
 package main
 
 import (
@@ -14,14 +14,14 @@ import (
 
 func init() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler)
-	r.HandleFunc("/index", IndexHandler)
-	r.HandleFunc("/roll/{roll}", RollHandler)
-	r.HandleFunc("/slack/roll/{roll}", SlackHandler)
+	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/index", indexHandler)
+	r.HandleFunc("/roll/{roll}", rollHandler)
+	r.HandleFunc("/slack/roll/{roll}", slackHandler)
 	http.Handle("/", r)
 }
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	mtemplate.RenderFile("html/index.html", w, nil)
 }
 
@@ -45,7 +45,7 @@ func doRollRequest(w http.ResponseWriter, r *http.Request) (*roller.RollResults,
 	return &rollResult, true
 }
 
-func RollHandler(w http.ResponseWriter, r *http.Request) {
+func rollHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	results, ok := doRollRequest(w, r)
@@ -66,20 +66,20 @@ func RollHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-const slack_token string = "IbAZE0ckwoSNJcsGWE7sqX5j"
+const slackToken string = "IbAZE0ckwoSNJcsGWE7sqX5j"
 
 type slackResponse struct {
 	Text         string `json:"text"`
 	ResponseType string `json:"response_type"`
 }
 
-func SlackHandler(w http.ResponseWriter, r *http.Request) {
+func slackHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	foundToken := r.PostFormValue("token")
 
 	// Make sure that this is a legit request from Slack
-	if foundToken == slack_token {
+	if foundToken == slackToken {
 		// Make sure that we have a repsonse url to which to write
 		respondTo := r.PostFormValue("response_url")
 		if respondTo != "" {
